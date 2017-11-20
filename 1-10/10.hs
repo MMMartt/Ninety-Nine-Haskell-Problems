2616b15feb064386cb24bc7a98d12f361e9560d7
MartinNey <nielumartin@hotmail.com>
2017-11-20 15:18:20 +0800

    update: description of 10

--- 1-10/10.hs
+++ 1-10/10.hs
@@ -1,4 +1,4 @@
--- (**) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+-- (*) Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
 module Main
   ( main
   ) where
